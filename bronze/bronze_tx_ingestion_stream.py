2e3442180ae00e386222274b1febd45fc7789cb1
italianriver <v.tibor.work@gmail.com>
2025-11-26 13:55:07 +0100

    Update bronze_tx_ingestion_stream.py

--- bronze/bronze_tx_ingestion_stream.py
+++ bronze/bronze_tx_ingestion_stream.py
@@ -2,10 +2,10 @@ from pyspark.sql import functions as F
 
 #Spark structured streaming ingestion, from transactions json source → bronze transactions delta table
 
-input_path = '/Workspace/Users/v.tibor+dbx1@gmail.com/fraud_detection/source/generated_transactions' #transaction source path
-output_path = 'fraud_detection.bronze.bronze_transactions'                                           #output table path
-checkpoint_path = '/Volumes/fraud_detection/bronze/bronze_checkpoints'                               #checkpoints are saved here
-schema_path = '/Workspace/Users/v.tibor+dbx1@gmail.com/fraud_detection/bronze/tx_ingestion_schemas'  #schema location
+input_path = 'path/to/transaction_source'                             #transaction source path
+output_path = 'fraud_detection.bronze.bronze_transactions'            #output table path
+checkpoint_path = 'path/to/checkpoints'                               #checkpoints are saved here
+schema_path = 'path/to/schema'                                        #schema location
 
 df = (spark.readStream
     .format('cloudFiles')
